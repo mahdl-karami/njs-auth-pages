ffd9764959411e7bccd5f12617e91658cd161594
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-12-06 20:06:33 +0330

    add visibility-eye for password

--- components/Form.js
+++ components/Form.js
@@ -1,24 +1,29 @@
 import ChangeHandler from "@/helpers/ChangeHandler";
 import { useEffect, useState } from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
-
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 function Form({ formType }) {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const [passVisibility, setPassVisibility] = useState(false);
   useEffect(() => {
     setFormData({
       email: "",
       password: "",
     });
+    setPassVisibility(false);
   }, [formType]);
   return (
     <form className="form" onChange={(ev) => ChangeHandler(ev, setFormData)}>
       <h3>{formType == "SignUp" ? "Create Account" : "Login To Account"}</h3>
       <input value={formData.email} name="email" placeholder="Email" type="email" />
-      <div>
-        <input value={formData.password} name="password" placeholder="Password" type="password"></input>
+      <div className="visibility">
+        <input value={formData.password} name="password" placeholder="Password" type={passVisibility ? "text" : "password"}></input>
+        <button type="button" onClick={() => setPassVisibility((prevS) => !prevS)}>
+          {passVisibility ? <AiFillEye /> : <AiFillEyeInvisible />}
+        </button>
       </div>
       <a href="#">Need Help ? </a>
       <button className="submit-btn">
